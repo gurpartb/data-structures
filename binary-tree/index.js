12b43f72f556aa4b85a4bcfd4f6280972b7fd03a
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-03-25 19:32:50 -0600

    implemented binary tree, add, print methods

---
+++ binary-tree/index.js
@@ -0,0 +1,62 @@
+class BinaryTree{
+
+    constructor(data){
+        this.data = data;
+        this.left;
+        this.right;
+    }
+
+    add(data){
+
+        if(!this.data){
+            this.data = data;
+        }
+
+        if(data < this.data){
+
+            if(!this.left){
+                this.left = new BinaryTree(data);
+            }else{
+                this.left.add(data);
+            }
+        }
+
+        if(data > this.data){
+
+            if(!this.right){
+                this.right = new BinaryTree(data);
+            } else {
+                this.right.add(data);
+            }
+        }
+    }
+
+    print(){
+
+        if(!this.data){
+            return;
+        }
+
+        if(this.left){
+            this.left.print();
+        }
+            
+        process.stdout.write(this.data.toString()+' ');
+    
+        if(this.right){
+            this.right.print();
+        }
+    }
+}
+
+let b = new BinaryTree(3);
+b.add(4);
+b.add(1);
+b.print()
+console.log()
+b.add(7);
+b.print();
+console.log();
+b.add(-3);
+b.print()
+console.log();
